681b2000233f6abc84584645b0883335b4fad76e
caviar <cjm191207@gmail.com>
2023-09-28 18:08:43 +0800

    使用线程池处理accept函数

--- socket_threadpool/server.c
+++ socket_threadpool/server.c
@@ -3,9 +3,25 @@
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <pthread.h> // 线程相关的头文件
+#include "threadpool.h" // 线程池相关的头文件
+
+struct SockInfo
+{
+    int fd;
+    struct sockaddr_in addr;
+};
+
+typedef struct PoolInfo
+{
+    int fd;
+    ThreadPool* p;
+}PoolInfo;
+
+void working(void* arg); // 线程的工作函数
+void acceptConn(void* arg); // 线程的工作函数
 
 int main(){
-
     // 1. 创建监听的socket
     int fd = socket(AF_INET, SOCK_STREAM, 0); // ipv4, tcp
     if(fd == -1){
@@ -31,27 +47,53 @@ int main(){
         return -1;
     }
 
+    // 创建线程池
+    ThreadPool *pool = threadPoolCreate(3, 8, 100);
+    PoolInfo* info = (PoolInfo*)malloc(sizeof(PoolInfo));
+    info->fd = fd;
+    info->p = pool;
+    threadPoolAdd(pool, acceptConn, info); // 添加任务
+
+    pthread_exit(NULL);
+    return 0;
+}
+
+void acceptConn(void* arg){
+    // 1. 获取参数
+    PoolInfo* poolInfo = (PoolInfo*)arg;
     // 4. 阻塞并等待客户端连接
-    struct sockaddr_in caddr; // 客户端的ip和端口
-    int addrlen = sizeof(caddr);
-    int cfd = accept(fd, (struct sockaddr*)&caddr, (socklen_t*)&addrlen); // 阻塞
-    if(cfd == -1){
+    int addrlen = sizeof(struct sockaddr_in);
+    while (1){    
+        struct SockInfo* pinfo = NULL;
+        pinfo = (struct SockInfo*)malloc(sizeof(struct SockInfo));
+        pinfo->fd = accept(poolInfo->fd, (struct sockaddr*)&pinfo->addr, &addrlen); 
+        if(pinfo->fd == -1){
         perror("accept");
-        return -1;
+        break;
+        }
+        // 创建通信任务
+        threadPoolAdd(poolInfo->p, working, pinfo);
     }
 
+    close(poolInfo->fd);
+}
+
+void working(void* arg){
+
+    struct SockInfo* pinfo = (struct SockInfo*)arg;
     // 连接成功，打印客户端的ip和端口
     char ip[32];
-    printf("client ip: %s, port: %d\n", inet_ntop(AF_INET, &caddr.sin_addr.s_addr, ip, sizeof(ip)), ntohs(caddr.sin_port));
+    printf("client ip: %s, port: %d\n", 
+            inet_ntop(AF_INET, &pinfo->addr.sin_addr.s_addr, ip, sizeof(ip)), 
+            ntohs(pinfo->addr.sin_port));
 
     // 5. 通信
-    while (1)
-    {
+    while (1){
         char buf[1024];
-        int len = recv(cfd, buf, sizeof(buf), 0); // 阻塞
+        int len = recv(pinfo->fd, buf, sizeof(buf), 0); // 阻塞
         if(len > 0){
             printf("client say: %s\n", buf);
-            send(cfd, buf, len, 0);
+            send(pinfo->fd, buf, len, 0);
         }else if(len == 0){
             printf("client closed...\n");
             break;
@@ -62,8 +104,5 @@ int main(){
     }
     
     // 6. 关闭socket
-    close(cfd);
-    close(fd);
-
-    return 0;
+    close(pinfo->fd);
 }
